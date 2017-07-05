52440b43e0d4956135d014eda3b288e98f4bf6df
Evgeny <evgeny@batyukov.ru>
2017-07-05 05:42:34 +0000

    Clearing

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,7 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'CrudGeneratorTest' => $vendorDir . '/appzcoder/crud-generator/tests/CrudGeneratorTest.php',
-    'Kernel' => $vendorDir . '/appzcoder/crud-generator/tests/Kernel.php',
-    'TestCase' => $vendorDir . '/appzcoder/crud-generator/tests/TestCase.php',
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,166 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit3900937257660f4f7b23a716197bc0b5
 {
-    public static $files = array (
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
-        '72579e7bd17821bb1321b87411366eae' => __DIR__ . '/..' . '/illuminate/support/helpers.php',
-        'f18cc91337d49233e5754e93f3ed9ec3' => __DIR__ . '/..' . '/laravelcollective/html/src/helpers.php',
-    );
-
     public static $prefixLengthsPsr4 = array (
-        'S' => 
-        array (
-            'Symfony\\Polyfill\\Mbstring\\' => 26,
-            'Symfony\\Component\\Translation\\' => 30,
-            'Symfony\\Component\\Routing\\' => 26,
-            'Symfony\\Component\\HttpKernel\\' => 29,
-            'Symfony\\Component\\HttpFoundation\\' => 33,
-            'Symfony\\Component\\Finder\\' => 25,
-            'Symfony\\Component\\EventDispatcher\\' => 34,
-            'Symfony\\Component\\Debug\\' => 24,
-        ),
-        'P' => 
-        array (
-            'Psr\\Log\\' => 8,
-        ),
-        'I' => 
-        array (
-            'Illuminate\\View\\' => 16,
-            'Illuminate\\Support\\' => 19,
-            'Illuminate\\Session\\' => 19,
-            'Illuminate\\Routing\\' => 19,
-            'Illuminate\\Pipeline\\' => 20,
-            'Illuminate\\Http\\' => 16,
-            'Illuminate\\Filesystem\\' => 22,
-            'Illuminate\\Events\\' => 18,
-            'Illuminate\\Contracts\\' => 21,
-            'Illuminate\\Container\\' => 21,
-        ),
-        'C' => 
-        array (
-            'Collective\\Html\\' => 16,
-            'Carbon\\' => 7,
-        ),
         'B' => 
         array (
             'BatyukovArt\\CrudGeneratorTemplates\\' => 35,
         ),
-        'A' => 
-        array (
-            'Appzcoder\\CrudGenerator\\' => 24,
-        ),
     );
 
     public static $prefixDirsPsr4 = array (
-        'Symfony\\Polyfill\\Mbstring\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
-        ),
-        'Symfony\\Component\\Translation\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/translation',
-        ),
-        'Symfony\\Component\\Routing\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/routing',
-        ),
-        'Symfony\\Component\\HttpKernel\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/http-kernel',
-        ),
-        'Symfony\\Component\\HttpFoundation\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/http-foundation',
-        ),
-        'Symfony\\Component\\Finder\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/finder',
-        ),
-        'Symfony\\Component\\EventDispatcher\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/event-dispatcher',
-        ),
-        'Symfony\\Component\\Debug\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/debug',
-        ),
-        'Psr\\Log\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
-        ),
-        'Illuminate\\View\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/view',
-        ),
-        'Illuminate\\Support\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/support',
-        ),
-        'Illuminate\\Session\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/session',
-        ),
-        'Illuminate\\Routing\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/routing',
-        ),
-        'Illuminate\\Pipeline\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/pipeline',
-        ),
-        'Illuminate\\Http\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/http',
-        ),
-        'Illuminate\\Filesystem\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/filesystem',
-        ),
-        'Illuminate\\Events\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/events',
-        ),
-        'Illuminate\\Contracts\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/contracts',
-        ),
-        'Illuminate\\Container\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/container',
-        ),
-        'Collective\\Html\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/laravelcollective/html/src',
-        ),
-        'Carbon\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon',
-        ),
         'BatyukovArt\\CrudGeneratorTemplates\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
         ),
-        'Appzcoder\\CrudGenerator\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/appzcoder/crud-generator/src',
-        ),
-    );
-
-    public static $prefixesPsr0 = array (
-        'D' => 
-        array (
-            'Doctrine\\Common\\Inflector\\' => 
-            array (
-                0 => __DIR__ . '/..' . '/doctrine/inflector/lib',
-            ),
-        ),
-    );
-
-    public static $classMap = array (
-        'CrudGeneratorTest' => __DIR__ . '/..' . '/appzcoder/crud-generator/tests/CrudGeneratorTest.php',
-        'Kernel' => __DIR__ . '/..' . '/appzcoder/crud-generator/tests/Kernel.php',
-        'TestCase' => __DIR__ . '/..' . '/appzcoder/crud-generator/tests/TestCase.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -173,8 +25,6 @@ class ComposerStaticInit3900937257660f4f7b23a716197bc0b5
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit3900937257660f4f7b23a716197bc0b5::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit3900937257660f4f7b23a716197bc0b5::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit3900937257660f4f7b23a716197bc0b5::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit3900937257660f4f7b23a716197bc0b5::$classMap;
 
         }, null, ClassLoader::class);
     }
